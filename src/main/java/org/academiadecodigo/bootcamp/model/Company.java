621862d25c9b50405d08c990cc166843a412c626
Joao Martins <joaopaulomartinspro@gmail.com>
2018-01-25 19:53:26 -0100

    added Individuals and Company Models

--- src/main/java/org/academiadecodigo/bootcamp/model/Company.java
+++ src/main/java/org/academiadecodigo/bootcamp/model/Company.java
@@ -1,4 +1,17 @@
 package org.academiadecodigo.bootcamp.model;
 
 public class Company extends User {
+    private int company_id;
+
+    public Company(int company_id) {
+        this.company_id = company_id;
+    }
+
+    public int getCompany_id() {
+        return company_id;
+    }
+
+    public void setCompany_id(int company_id) {
+        this.company_id = company_id;
+    }
 }
--- src/main/java/org/academiadecodigo/bootcamp/model/Individual.java
+++ src/main/java/org/academiadecodigo/bootcamp/model/Individual.java
@@ -1,4 +1,30 @@
 package org.academiadecodigo.bootcamp.model;
 
+import java.util.Date;
+
 public class Individual extends User {
+    private int individual_id;
+    private Date date_of_birth;
+
+    public Individual(int individual_id, Date date_of_birth) {
+        this.individual_id = individual_id;
+        this.date_of_birth = date_of_birth;
+    }
+
+    public int getIndividual_id() {
+        return individual_id;
+    }
+
+    public void setIndividual_id(int individual_id) {
+        this.individual_id = individual_id;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
 }
+
